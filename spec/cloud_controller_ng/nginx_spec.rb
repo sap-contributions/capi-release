7ed11a1311ee32e5a0dcf28b7a0715a5f05f89fc
Michael Oleske <moleske@pivotal.io>
2022-07-11 21:40:33 +0000

    Conditionally render the prom scraper nginx server when the certs are available
    Co-authored-by: Michael Oleske <moleske@pivotal.io>
    Co-authored-by: David Alvarado <alvaradoda@vmware.com>

--- spec/cloud_controller_ng/nginx_spec.rb
+++ spec/cloud_controller_ng/nginx_spec.rb
@@ -32,6 +32,38 @@ module Bosh
           end
         end
       end
+
+      context 'prom_scraper with all propeties set' do
+        let(:manifest_properties) { { 'cc' => { 'prom_scraper_tls' => { 'public_cert' => 'a public cert', 'private_key' => 'a private key', 'ca_cert' => 'an authority'  } } } }
+
+        it 'renders prom scraper server' do
+          expect(@rendered_file).to include('include prom_scraper_mtls.conf')
+        end
+      end
+
+      context 'prom_scraper with public_cert not set' do
+        let(:manifest_properties) { { 'cc' => { 'prom_scraper_tls' => { 'private_key' => 'a private key', 'ca_cert' => 'an authority'  } } } }
+
+        it 'does not render prom scraper server' do
+          expect(@rendered_file).not_to include('include prom_scraper_mtls.conf')
+        end
+      end
+
+      context 'prom_scraper with private_key not set' do
+        let(:manifest_properties) { { 'cc' => { 'prom_scraper_tls' => { 'public_cert' => 'a public cert', 'ca_cert' => 'an authority'  } } } }
+
+        it 'does not render prom scraper server' do
+          expect(@rendered_file).not_to include('include prom_scraper_mtls.conf')
+        end
+      end
+
+      context 'prom_scraper with ca_cert not set' do
+        let(:manifest_properties) { { 'cc' => { 'prom_scraper_tls' => { 'public_cert' => 'a public cert', 'private_key' => 'a private key'  } } } }
+
+        it 'does not render prom scraper server' do
+          expect(@rendered_file).not_to include('include prom_scraper_mtls.conf')
+        end
+      end
     end
   end
 end
