ef2f302801e21952f4f14e574a56d408a4ba55e9
Katharina Przybill <30441792+kathap@users.noreply.github.com>
2025-01-09 14:07:39 +0100

    Draining CC-api VMs should let local-worker jobs finish (#496)
    * Draining CC-api VMs should let local-worker jobs finish
    
    * add test
    
    * add 2. test

--- spec/cloud_controller_ng/drain_spec.rb
+++ spec/cloud_controller_ng/drain_spec.rb
@@ -19,6 +19,20 @@ module Bosh
             expect(rendered_file).to include("@drain.shutdown_nginx('/var/vcap/sys/run/bpm/cloud_controller_ng/nginx.pid', 30)")
           end
 
+          context "when 'local_worker_grace_period_seconds' is provided" do
+            it 'renders the provided value' do
+              rendered_file = template.render({ 'cc' => { 'jobs' => { 'local' => { 'worker_grace_period_seconds' => 300 } } } }, consumes: {})
+              expect(rendered_file).to include('@local_worker_grace_period_seconds = 300')
+            end
+          end
+
+          context "when 'local.number_of_workers' is provided" do
+            it 'renders the provided number of workers' do
+              rendered_file = template.render({ 'cc' => { 'jobs' => { 'local' => { 'number_of_workers' => 5 } } } }, consumes: {})
+              expect(rendered_file).to include('(1..5).each do |i|')
+            end
+          end
+
           context 'when nginx timeout is provided' do
             it 'renders the provided value' do
               rendered_file = template.render({ 'cc' => { 'nginx_drain_timeout' => 60 } }, consumes: {})
